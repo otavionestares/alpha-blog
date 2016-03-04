3cf3787bd18d9b14e98679dfa5a91a0f917a9d54
otavionestares <otavio.nestares@gmail.com>
2016-03-04 19:44:02 +0000

    Extract redundancy and prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,17 @@
 class ArticlesController < ApplicationController
     
+    
+    def index
+        @articles = Article.all
+    end
+    
     def new
       @article = Article.new    
+        
+    end
+    
+    def edit
+      @article = Article.find(params[:id])    
         
     end
     
@@ -14,14 +24,31 @@ class ArticlesController < ApplicationController
       else
         render 'new'    
       end
-      
-      
     end
+    
+    def update
+      @article = Article.find(params[:id])
+      if @article.update(article_params)
+        flash[:notice] = 'Article updated'  
+        redirect_to article_path(@article)
+      else 
+          render 'edit'
+      end
+    end
+    
     
     def show
         
        @article = Article.find(params[:id]) 
         
+    end
+    
+    def destroy
+      @article = Article.find(params[:id])
+      @article.destroy
+      flash[:notice] = 'Article sucessfully deleted'
+      redirect_to articles_path
+      
     end
     
     private 
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,9 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+    
     root 'pages#home'
-    get 'about', to: 'pages#about'
+    get 'pages/about' => 'pages#about', as: :about
     resources :articles
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
